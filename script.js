e6545853a95dda72161652bd57d0d34ca8be1537
ferfrrz <161397960+ferfrrz@users.noreply.github.com>
2025-08-25 21:50:31 +0000

    perguntas 2.0

--- script.js
+++ script.js
@@ -58,15 +58,15 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você se considera mais:",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "afirmação"
+                texto: "Realista.",
+                afirmacao: "Você lida bem como  a verdade, prefere os fatos e é pé no chão."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "afirmação"
+                texto: "Sonhador.",
+                afirmacao: "Você tem uma mente criativa, vive com esperança e vê além do agora."
             }
         ]
     },
